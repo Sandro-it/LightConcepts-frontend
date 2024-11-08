80177650b9de668b44260bef4f0155affb8c1375
Sandro-it <kozachenkooleksandr@gmail.com>
2024-11-08 23:36:07 +0200

    PC-PP-scrol

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,8 +1,56 @@
+// import { Link } from "react-router-dom";
+// import ProductList from "../components/ProductList";
+// import styles from "../styles/ProductsPage.module.css";
+
+// const ProductsPage = () => {
+//   return (
+//     <div className={styles.productsPage}>
+//       <h2>Всі товари</h2>
+//       <div className={styles.categoryLinks}>
+//         <Link to="/lights-category" className={styles.categoryLink}>
+//           Світильники
+//         </Link>
+//         <Link to="/candles-category" className={styles.categoryLink}>
+//           Свічки
+//         </Link>
+//         <Link to="/furniture-category" className={styles.categoryLink}>
+//           Меблі
+//         </Link>
+//       </div>
+//       <ProductList />
+//     </div>
+//   );
+// };
+
+// export default ProductsPage;
+
+//=================================================================================//
+
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import ProductList from "../components/ProductList";
 import styles from "../styles/ProductsPage.module.css";
 
 const ProductsPage = () => {
+  const [showScrollToTop, setShowScrollToTop] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 300) {
+        setShowScrollToTop(true);
+      } else {
+        setShowScrollToTop(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <div className={styles.productsPage}>
       <h2>Всі товари</h2>
@@ -18,6 +66,10 @@ const ProductsPage = () => {
         </Link>
       </div>
       <ProductList />
+      {/* Кнопка "Повернутися на початок" */}
+      {showScrollToTop && (
+        <button className={styles.scrollToTop} onClick={scrollToTop}></button>
+      )}
     </div>
   );
 };
